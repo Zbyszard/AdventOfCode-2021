4ab7f46c7a1d9e0134fefee05141853e763527a7
zbyszard <florcmat@gmail.com>
2021-12-10 17:49:25 +0100

    refactored Program.cs

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,28 +1,10 @@
-﻿using AdventOfCode;
+﻿foreach (int i in Enumerable.Range(1, 25))
+{
+    string className = "AdventOfCode.D" + i;
+    var type = Type.GetType(className);
+    if (type is null)
+        break;
 
-Console.WriteLine(D1.SolveA());
-Console.WriteLine(D1.SolveB());
-
-Console.WriteLine(D2.SolveA());
-Console.WriteLine(D2.SolveB());
-
-Console.WriteLine(D3.SolveA());
-Console.WriteLine(D3.SolveB());
-
-Console.WriteLine(D4.SolveA());
-Console.WriteLine(D4.SolveB());
-
-Console.WriteLine(D5.SolveA());
-Console.WriteLine(D5.SolveB());
-
-Console.WriteLine(D6.SolveA());
-Console.WriteLine(D6.SolveB());
-
-Console.WriteLine(D7.SolveA());
-Console.WriteLine(D7.SolveB());
-
-Console.WriteLine(D8.SolveA());
-Console.WriteLine(D8.SolveB());
-
-Console.WriteLine(D9.SolveA());
-Console.WriteLine(D9.SolveB());
+    Console.WriteLine(type?.GetMethod("SolveA")!.Invoke(null, null));
+    Console.WriteLine(type?.GetMethod("SolveB")!.Invoke(null, null));
+}
